a20e836c37a047f168afc96314da4e713e8f3db8
alex <sv5060@gmail.com>
2018-05-28 17:50:25 +0900

    init

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,30 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         // filename: '[name].[chunkhash].min.js'
         // publicPath: '/js/',
-        filename: '[name].min.js',
+        filename: '[name].js',
     },
     plugins: [
         new WebpackCleanupPlugin(),
         // new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
-        new UglifyJSPlugin()
+        // new UglifyJSPlugin()
     ],
     resolve: {
         modules: ['node_modules'],
         extensions: ['.js'],
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname),
+                exclude: /(node_modules)|(dist)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
 }
